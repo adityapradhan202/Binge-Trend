5fa375953210d2d0e76d5d5683083842301d6585
adityapradhan202 <adityapradhan5060@gmail.com>
2025-04-05 10:37:59 +0530

    Debugging and testing of fine_tune function in the util package is done, also some minor unsaved changes in notebook movie1

--- ml_backend/notebooks/utils/fine_tune.py
+++ ml_backend/notebooks/utils/fine_tune.py
@@ -1,6 +1,8 @@
-# Function for training specific algorithms, also fine tuning them and comparing to the base form of algorithms...
-# Work pending here...
+# Function for training specific algorithms
+# Also fine tuning them and comparing to the base form of algorithms...
 
+import numpy as np
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report
@@ -9,13 +11,32 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import RandomizedSearchCV
 
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.linear_model import LogisticRegression
+
 class FineTuneError(Exception):
     def __init__(self, message):
         self.message = message
         super().__init__(self.message)
 
+# Train and Fine tune if needed
+# This for tfidf or countvectorizer method only
 def train_fine_tune(models, df, X, y, params_list, tsize=0.20, rstate=45, vec_type="tfidf", search_type="grid"):
-    """Trains specific machine learning algorithms, also fine tunes the algorithms, and if the fine tuned version of the algorithms peforms better then it returns those models, otherwise returns the base version of the algorithms."""
+    """
+    Trains specific machine learning algorithms, also fine tunes the algorithms, and if the fine tuned version of the algorithms peforms better then it returns those models, otherwise returns the base version of the algorithms.
+    Args:
+        models: list of machine learning algorithms
+        df: pandas dataframe
+        X: string of feature column, description or synopsis column here
+        y: string of label column
+        param_list: a list of parameter grids that we use in search techniques
+        tsize: test size for train test split
+        rstate: random state for train test split
+        vec_type: default is "tfidf", for TfIdfVectorizer, and "count" for CountVectorizer
+        search_type: default is "grid", for GridSearchCV, and "random" for "RandomizedSearchCv"
+    Returns:
+        filt_models: best models out of both the base algorithms and fine tuned algorithms
+    """
 
     try:
         if len(models) != len(params_list):
@@ -63,12 +84,13 @@ def train_fine_tune(models, df, X, y, params_list, tsize=0.20, rstate=45, vec_ty
         perf_base[str(model)] = acc
     
     try:
-        if search_type != "grid" or search_type != "random":
-            raise FineTuneError("Enter a valid string for chooisng the string technique!")
+        if not (search_type == "grid" or search_type == "random"):
+            raise FineTuneError("Enter a valid string for chooisng the search technique!")
     except Exception as e:
         print(f"Exception: {e}")
         return # Terminate
     
+    print("\nPlease wait for some time, search technique is finding the best hyper parameters...")
     print(f"\n\n---> Tune models:")
     for ind, model in enumerate(models):
         if search_type == "grid":
@@ -78,16 +100,26 @@ def train_fine_tune(models, df, X, y, params_list, tsize=0.20, rstate=45, vec_ty
                 scoring="accuracy",
                 n_jobs=4, cv=5
             )
-            model_tune.fit(spX_train, y_train)
-            preds_tune = model_tune.predict(spX_test)
-            acc_tune = accuracy_score(y_test, preds_tune)
-            creport_tune = classification_report(y_test, preds_tune)
-            print(f"\nModel: {model_tune}")
-            print(f"\nClassification report:\n{creport_tune}")
-            print(f"\nOverall accuracy: {acc_tune}")
+        elif search_type == "random":
+            model_tune = RandomizedSearchCV(
+                estimator=model,
+                param_distributions=params_list[ind],
+                scoring="accuracy",
+                cv=5,
+                n_jobs=4,
+                n_iter=10
+            )
+               
+        model_tune.fit(spX_train, y_train)
+        preds_tune = model_tune.predict(spX_test)
+        acc_tune = accuracy_score(y_test, preds_tune)
+        creport_tune = classification_report(y_test, preds_tune)
+        print(f"\nModel: {model_tune}")
+        print(f"\nClassification report:\n{creport_tune}")
+        print(f"\nOverall accuracy: {acc_tune}")
 
-            tune_models.append(model_tune)
-            perf_tune[str(model)] = acc_tune
+        tune_models.append(model_tune)
+        perf_tune[str(model)] = acc_tune
 
     print("---> Comparing and selecting the best algorithms:")
     model_str = list(perf_base.keys())
@@ -104,8 +136,39 @@ def train_fine_tune(models, df, X, y, params_list, tsize=0.20, rstate=45, vec_ty
     return filt_models
 
 if __name__ == "__main__":
-    pass
-    # Test the function here!
+    # Testing the function
+    df = pd.read_excel("D:/projects-aiml/Binge-Trend/ml_backend/data_sets/rotten_tomatoes_100.xlsx")
+    df = df[(df["label"]=="romantic") | (df["label"]=="horror")]
+    filt_models = train_fine_tune(
+        models=[
+            AdaBoostClassifier(algorithm="SAMME"),
+            LogisticRegression(max_iter=10000)
+        ],
+        df=df,
+        X="synopsis", y="label",
+        params_list=[
+            {
+                "n_estimators":[50,100,150,200],
+            },
+            {
+                "C":np.logspace(1,10,20), # Intensity of l1,l2
+                "class_weight":["balanced", None],
+                "solver":["saga"], # For performing multiclass classification
+                "penalty":["l1","l2"]
+            }
+        ],
+        tsize=0.20,
+        rstate=45,
+        vec_type="tfidf",
+        search_type="grid"   
+    )
+
+    print("\nThese are the filtered models:")
+    print(filt_models)
+    print("\nThese are the best params of the model:")
+    for model in filt_models:
+        print(f"Model name: {model}")
+        print(f"Best params:\n{model.get_params()}\n")
 
     
 
